3e6a635aeed62afd20a6876122ad9dad5b4b8c8c
cicciord <102993057+cicciord@users.noreply.github.com>
2024-10-08 16:27:14 +0200

    config hardhat chain on oracle server

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,13 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    oracle: {
+      url: "https://radica.dev:8545",
+    },
+  },
 };
 
 export default config;
