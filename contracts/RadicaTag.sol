0b8df60d39cc61a095ccbec5dbd923a2ed03bb5b
Francesco Laterza <francescolaterza00@gmail.com>
2024-12-04 18:07:33 +0100

    custom contract for RadicaTag and Metadata on chain

--- contracts/RadicaTag.sol
+++ contracts/RadicaTag.sol
@@ -2,9 +2,6 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -21,20 +18,26 @@ struct Metadata {
     string externalUrl;
 }
 
+struct Certificate {
+    uint256 id;
+    Metadata metadata;
+}
+
 /**
  * @title RadicaTag
  * @author Francesco Laterza
  * @notice Contract implementation to manage Radica NFC tags
  */
-contract RadicaTag is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+contract RadicaTag is Ownable {
     using Strings for string;
     using Base64 for bytes;
 
     address public radicaPropertyAddr;
-
     uint256 private _nextTokenId;
 
-    constructor() ERC721("RadicaTag", "RTAG") Ownable(msg.sender) {}
+    mapping(address => Certificate) public tagAddrToCert;
+
+    constructor() Ownable(msg.sender) {}
 
     /**
      * @notice Sets the RadicaProperty contract address
@@ -65,129 +68,104 @@ contract RadicaTag is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         bytes32 proofHash
     ) public onlyOwner {
         require(proofHash != 0, "Proof hash cannot be zero");
-        require(balanceOf(tagAddr) == 0, "Tag already in use");
+        require(tagAddrToCert[tagAddr].id == 0, "Tag already in use");
 
-        string memory metadataStr = _serializaMetadata(metadata);
-        string memory uri = _formatURI(metadataStr);
+        uint256 certId = _deriveCertId(msg.sender, tagAddr);
 
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(tagAddr, tokenId);
-        _setTokenURI(tokenId, uri);
+        tagAddrToCert[tagAddr] = Certificate({id: certId, metadata: metadata});
 
         (bool success, ) = radicaPropertyAddr.call(
             abi.encodeWithSignature(
                 "setProof(uint256,bytes32)",
-                tokenId,
+                certId,
                 proofHash
             )
         );
         require(success, "Call to setProof failed");
     }
 
-    function _serializaMetadata(
-        Metadata memory metadata
-    ) private pure returns (string memory metadataStr) {
-        require(!_isEmptyStr(metadata.id), "ID cannot be empty");
+    function _deriveCertId(
+        address issuerAddr,
+        address tagAddr
+    ) private pure returns (uint256) {
+        uint256 issuerAddrUint = uint256(uint160(issuerAddr));
+        uint256 tagAddrUint = uint256(uint160(tagAddr));
 
-        metadataStr = "{";
+        uint256 issuerFP = issuerAddrUint >> 64;
 
-        if (!_isEmptyStr(metadata.name)) {
-            metadataStr = string(
-                abi.encodePacked(metadataStr, '"name":"', metadata.name, '",')
-            );
-        }
-
-        if (!_isEmptyStr(metadata.description)) {
-            metadataStr = string(
-                abi.encodePacked(
-                    metadataStr,
-                    '"description":"',
-                    metadata.description,
-                    '",'
-                )
-            );
-        }
-
-        if (!_isEmptyStr(metadata.imageUrl)) {
-            metadataStr = string(
-                abi.encodePacked(
-                    metadataStr,
-                    '"image":"',
-                    metadata.imageUrl,
-                    '",'
-                )
-            );
-        }
-
-        if (!_isEmptyStr(metadata.externalUrl)) {
-            metadataStr = string(
-                abi.encodePacked(
-                    metadataStr,
-                    '"external_url":"',
-                    metadata.externalUrl,
-                    '",'
-                )
-            );
-        }
-
-        metadataStr = string(
-            abi.encodePacked(
-                metadataStr,
-                '"attributes":[{"trait_type":"ID","value":"',
-                metadata.id,
-                '"}]}'
-            )
-        );
+        return (issuerFP << 160) | tagAddrUint;
     }
 
-    function _formatURI(
-        string memory metadata
-    ) private pure returns (string memory) {
-        return
-            string(
-                abi.encodePacked(
-                    "data:application/json;base64,",
-                    Base64.encode(bytes(metadata))
-                )
-            );
-    }
-
-    function _isEmptyStr(string memory str) private pure returns (bool) {
-        return bytes(str).length == 0;
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _update(
-        address to,
-        uint256 tokenId,
-        address auth
-    ) internal override(ERC721, ERC721Enumerable) returns (address) {
-        require(_ownerOf(tokenId) == address(0), "Non transferable token"); // Only mint is allowed
-        return super._update(to, tokenId, auth);
-    }
-
-    function _increaseBalance(
-        address account,
-        uint128 value
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._increaseBalance(account, value);
-    }
-
-    function tokenURI(
-        uint256 tokenId
-    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(
-        bytes4 interfaceId
-    )
-        public
-        view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+    // function _serializaMetadata(
+    //     Metadata memory metadata
+    // ) private pure returns (string memory metadataStr) {
+    //     require(!_isEmptyStr(metadata.id), "ID cannot be empty");
+    //
+    //     metadataStr = "{";
+    //
+    //     if (!_isEmptyStr(metadata.name)) {
+    //         metadataStr = string(
+    //             abi.encodePacked(metadataStr, '"name":"', metadata.name, '",')
+    //         );
+    //     }
+    //
+    //     if (!_isEmptyStr(metadata.description)) {
+    //         metadataStr = string(
+    //             abi.encodePacked(
+    //                 metadataStr,
+    //                 '"description":"',
+    //                 metadata.description,
+    //                 '",'
+    //             )
+    //         );
+    //     }
+    //
+    //     if (!_isEmptyStr(metadata.imageUrl)) {
+    //         metadataStr = string(
+    //             abi.encodePacked(
+    //                 metadataStr,
+    //                 '"image":"',
+    //                 metadata.imageUrl,
+    //                 '",'
+    //             )
+    //         );
+    //     }
+    //
+    //     if (!_isEmptyStr(metadata.externalUrl)) {
+    //         metadataStr = string(
+    //             abi.encodePacked(
+    //                 metadataStr,
+    //                 '"external_url":"',
+    //                 metadata.externalUrl,
+    //                 '",'
+    //             )
+    //         );
+    //     }
+    //
+    //     metadataStr = string(
+    //         abi.encodePacked(
+    //             metadataStr,
+    //             '"attributes":[{"trait_type":"ID","value":"',
+    //             metadata.id,
+    //             '"}]}'
+    //         )
+    //     );
+    // }
+    //
+    // function _formatURI(
+    //     string memory metadata
+    // ) private pure returns (string memory) {
+    //     return
+    //         string(
+    //             abi.encodePacked(
+    //                 "data:application/json;base64,",
+    //                 Base64.encode(bytes(metadata))
+    //             )
+    //         );
+    // }
+    //
+    // function _isEmptyStr(string memory str) private pure returns (bool) {
+    //     return bytes(str).length == 0;
+    // }
 }
+
---
+++ test/RadicaTag.test.ts
@@ -0,0 +1,128 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import hre from "hardhat";
+import { expect } from "chai";
+import { getAddress } from "viem";
+
+describe("RadicaTag", function () {
+  async function deployRadicaTagFixture() {
+    const [owner, tag] = await hre.viem.getWalletClients();
+
+    const radicaTag = await hre.viem.deployContract("RadicaTag");
+
+    return {
+      radicaTag,
+      owner,
+      tag,
+    };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      const { radicaTag, owner } = await loadFixture(deployRadicaTagFixture);
+
+      expect(await radicaTag.read.owner()).to.equal(
+        getAddress(owner.account.address),
+      );
+    });
+  });
+
+  describe("Tag Creation", function () {
+    it("Should create a tag", async function () {
+      const { radicaTag, tag } = await loadFixture(deployRadicaTagFixture);
+
+      const tagAddr = tag.account.address;
+      const metadata = {
+        id: "1",
+        name: "test",
+        description: "test description",
+        imageUrl: "https://testimageurl.com",
+        externalUrl: "https://testexternalurl.com",
+      };
+      const proofHash =
+        `0x${Buffer.allocUnsafe(32).fill(1).toString("hex")}` as `0x${string}`;
+
+      await radicaTag.write.createTag([tagAddr, metadata, proofHash]);
+
+      const cert = await radicaTag.read.tagAddrToCert([tagAddr]);
+      expect(cert[0]).not.to.equal(0n);
+    });
+
+    it("Should set the correct certId to the Tag", async function () {
+      const { radicaTag, owner, tag } = await loadFixture(
+        deployRadicaTagFixture,
+      );
+
+      const ownerAddr = owner.account.address;
+      const tagAddr = tag.account.address;
+      const metadata = {
+        id: "1",
+        name: "test",
+        description: "test description",
+        imageUrl: "https://testimageurl.com",
+        externalUrl: "https://testexternalurl.com",
+      };
+      const proofHash =
+        `0x${Buffer.allocUnsafe(32).fill(1).toString("hex")}` as `0x${string}`;
+
+      await radicaTag.write.createTag([tagAddr, metadata, proofHash]);
+
+      const cert = await radicaTag.read.tagAddrToCert([tagAddr]);
+      const certId = cert[0];
+
+      const ownerAddrBigInt = BigInt(ownerAddr);
+      const tagAddrBigInt = BigInt(tagAddr);
+
+      const ownerFP = ownerAddrBigInt >> 64n;
+
+      const expectedCertId = (ownerFP << 160n) | tagAddrBigInt;
+
+      expect(certId).to.equal(expectedCertId);
+    });
+
+    it("Should set the correct metadata to the Tag", async function () {
+      const { radicaTag, tag } = await loadFixture(deployRadicaTagFixture);
+
+      const tagAddr = tag.account.address;
+      const metadata = {
+        id: "1",
+        name: "test",
+        description: "test description",
+        imageUrl: "https://testimageurl.com",
+        externalUrl: "https://testexternalurl.com",
+      };
+      const proofHash =
+        `0x${Buffer.allocUnsafe(32).fill(1).toString("hex")}` as `0x${string}`;
+
+      await radicaTag.write.createTag([tagAddr, metadata, proofHash]);
+
+      const cert = await radicaTag.read.tagAddrToCert([tagAddr]);
+      const metadataFromContract = cert[1];
+
+      expect(metadata.id).to.equal(metadataFromContract.id);
+      expect(metadata.name).to.equal(metadataFromContract.name);
+      expect(metadata.description).to.equal(metadataFromContract.description);
+      expect(metadata.imageUrl).to.equal(metadataFromContract.imageUrl);
+      expect(metadata.externalUrl).to.equal(metadataFromContract.externalUrl);
+    });
+
+    it("Should revert if the tag has already been used", async function () {
+      const { radicaTag, tag } = await loadFixture(deployRadicaTagFixture);
+
+      const tagAddr = tag.account.address;
+      const metadata = {
+        id: "1",
+        name: "test",
+        description: "test description",
+        imageUrl: "https://testimageurl.com",
+        externalUrl: "https://testexternalurl.com",
+      };
+      const proofHash =
+        `0x${Buffer.allocUnsafe(32).fill(1).toString("hex")}` as `0x${string}`;
+
+      await radicaTag.write.createTag([tagAddr, metadata, proofHash]);
+
+      expect(radicaTag.write.createTag([tagAddr, metadata, proofHash])).to
+        .rejected;
+    });
+  });
+});
